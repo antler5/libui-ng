99d47ee4a6588b1a211d8847cb6c50ed6bb68cac
antlers <antlers@illucid.net>
2024-05-01 16:25:52 -0700

    test

--- common/userbugs.c
+++ common/userbugs.c
@@ -4,5 +4,5 @@
 
 void uiUserBugCannotSetParentOnToplevel(const char *type)
 {
-	uiprivUserBug("You cannot make a %s a child of another uiControl,", type);
+	uiprivUserBug("You can't yet make a %s a child of another uiControl,", type);
 }
